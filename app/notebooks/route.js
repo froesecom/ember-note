b28f290769c8c5735eaf291751aaecf2dff4cd63
Erik Froese <froesecom@gmail.com>
2015-12-10 21:32:07 +1100

    add note model

--- app/notebooks/route.js
+++ app/notebooks/route.js
@@ -5,6 +5,18 @@ export default Ember.Route.extend({
     return this.store.query('notebook', {user: params.user_id})
   },
   actions: {
-    
+    addNotebook: function(){
+      var notebook = this.store.createRecord('notebook', {
+        title: this.controller.get('title'),
+        user: this.controllerFor('application').get('user')
+      });
+      notebook.save().then(() => {
+        console.log('save successful');
+        this.controller.set('title', null);
+        this.refresh();
+      }, function(){
+        console.log('save failed');
+      });
+    }
   }
 });
