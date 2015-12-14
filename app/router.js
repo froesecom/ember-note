5d7e5dfc73fbb6f96438b41ad79f4b4f839f1899
Erik Froese <froesecom@gmail.com>
2015-12-14 22:00:32 +1100

    add note route

--- app/router.js
+++ app/router.js
@@ -9,7 +9,9 @@ Router.map(function() {
   this.route('register');
   this.route('login');
   this.route('notebooks', {path:'notebooks/:user_id'}, function(){
-    this.route("notes", { path:"notes/:notebook_id"}, function() {});
+    this.route('notes', { path:'notes/:notebook_id'}, function() {
+      this.route('note', { path:'note/:note_id'},function(){});
+    });
   });
 });
 
