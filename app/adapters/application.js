9bc59a1f531fbd73e502d1dd3fa070991c6bed00
Erik Froese <froesecom@gmail.com>
2016-03-03 21:04:57 +1100

    create alternative api endpoint

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  namespace: 'api'
+  //Changing the namespace causes the adapter
+  //to send requests to an alternate API endpoint
+  namespace: 'api/alt'
 });
