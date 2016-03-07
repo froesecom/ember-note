23908472cf309760c43c8fe558299d3b6766939d
Erik Froese <froesecom@gmail.com>
2016-03-07 21:57:44 +1100

    add isValidEmail to register

--- app/register/route.js
+++ app/register/route.js
@@ -1,18 +1,26 @@
 import Ember from 'ember';
+import ValidationFunctions from 'ember-note/mixins/validation-functions';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ValidationFunctions, {
   actions: {
     addNew: function() {
-      var user = this.store.createRecord('user', {
-        name : this.controller.get('name')
-      });
-      user.save().then(() => {
-        console.log('save successful');
-        this.controller.set('message','A new user with the name "' + this.controller.get('name') + '" was added!');
-        this.controller.set('name',null);
-      }, function() {
-          console.log('save failed');
+      var name = this.controller.get('name');
+
+      if(this.isValidEmail(name)){
+        var user = this.store.createRecord('user', {
+          name : this.controller.get('name')
         });
+        user.save().then(() => {
+          console.log('save successful');
+          this.controller.set('message','A new user with the name "' + this.controller.get('name') + '" was added!');
+          this.controller.set('name',null);
+        }, function() {
+            console.log('save failed');
+          });
+      } else {
+        alert('Invalid email address');
+      }
+      
     }
   }
 });
